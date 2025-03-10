120ba50afe1bad8ca85f85c3448b20f2fb980924
Chris Vilches <felovilches@gmail.com>
2025-03-10 22:51:39 +0900

    fix autocmds file (make it simple and add one regarding nvim-tree being the last window), simplify <leader>q and add <leader>z to use toggle fullscreen custom-plugin

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -10,29 +10,14 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
--- Reload my custom theme automatically.
--- Should only work in this repo (configs repo).
--- From other repos, you can just re-execute the colorscheme command (and it will work
--- without having to exit neovim).
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = "nvim/lua/retroblue/*",
-  callback = function()
-    local curr_theme = vim.g.colors_name
+-- Close Neovim if nvim-tree is the only remaining window.
+vim.api.nvim_create_autocmd("WinClosed", {
+  callback = function(args)
+    local is_nvimtree = vim.bo[args.buf].filetype == "NvimTree"
+    local wins = vim.api.nvim_list_wins()
 
-    if not curr_theme:match "^retroblue" then
-      return
+    if #wins == 2 and not is_nvimtree and require("utils").is_nvimtree_open() then
+      vim.cmd ":qa"
     end
-
-    -- Remove from require cache.
-    for key, _ in pairs(package.loaded) do
-      if key:match "retroblue" then
-        package.loaded[key] = false
-      end
-    end
-
-    -- Not using vim.schedule() messes up the tabs (I think)
-    vim.schedule(function()
-      vim.cmd("colorscheme " .. curr_theme)
-    end)
   end,
 })
