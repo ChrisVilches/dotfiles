7b0af46b90744824ccf6325049937d4b1cd1614f
Chris Vilches <felovilches@gmail.com>
2025-03-11 21:24:15 +0900

    neovim: remove my crazy autocommand to close neovim if nvim-tree is the only remaining window (there are too many edge cases, like when a terminal is closed, etc)

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -9,16 +9,3 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.setpos(".", save_cursor)
   end,
 })
-
--- Close Neovim if nvim-tree is the only remaining window.
-vim.api.nvim_create_autocmd("WinClosed", {
-  callback = function(args)
-    local is_nvimtree = vim.bo[args.buf].filetype == "NvimTree"
-    local wins = vim.api.nvim_list_wins()
-
-    if #wins == 2 and not is_nvimtree and require("utils").is_nvimtree_open() then
-      vim.cmd ":SessionSave"
-      vim.cmd ":qa"
-    end
-  end,
-})
