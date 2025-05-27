522f9f40879c94f884a70555e081006a6985b09e
Chris Vilches <felovilches@gmail.com>
2025-05-27 20:46:28 +0900

    nvim: lsp, use a lazy event

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -12,8 +12,7 @@ end
 
 return {
   "neovim/nvim-lspconfig",
-  -- event = { "BufReadPre", "BufNewFile" },
-  lazy = false,
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
