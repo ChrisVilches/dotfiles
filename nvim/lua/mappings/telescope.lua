f00f2a1a4088bd696415ebdce4c3bb1cd7c2bee9
Chris Vilches <felovilches@gmail.com>
2025-10-21 23:31:06 +0900

    nvim: telescope refactor

--- nvim/lua/mappings/telescope.lua
+++ nvim/lua/mappings/telescope.lua
@@ -1,36 +1,34 @@
-local map = vim.keymap.set
+local function map(keymap, telescope_cmd, desc)
+  vim.keymap.set(
+    "n",
+    "<leader>" .. keymap,
+    "<cmd>Telescope " .. telescope_cmd .. "<CR>",
+    { desc = "Telescope: " .. desc }
+  )
+end
 
 -- More pickers: https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#pickers
-map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
-map("n", "<leader>fg", "<cmd>Telescope grep_string<CR>", { desc = "telescope grep string" })
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
-map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
-map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
-map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+map("fw", "live_grep", "live grep")
+map("fg", "grep_string", "grep string")
+map("fh", "help_tags", "help page")
+map("ma", "marks", "find marks")
+map("fo", "oldfiles", "find oldfiles")
+map("fz", "current_buffer_fuzzy_find", "find in current buffer")
+map("flb", "lsp_document_symbols", "Lists LSP document symbols in the current buffer")
+map("flw", "lsp_workspace_symbols", "Lists LSP document symbols in the current workspace")
+map("cm", "git_commits", "git commits")
+map("gt", "git_status", "git status")
+
 -- This kinda works (shows the full diagnostic message) but still truncates it most of the times.
 -- At least it's much better than before.
-map(
-  "n",
-  "<leader>ds",
-  "<cmd>:Telescope diagnostics wrap_results=true line_width=1000000<CR>",
-  { desc = "telescope diagnostics" }
-)
+map("ds", "diagnostics wrap_results=true line_width=1000000", "diagnostics")
+map("fa", "find_files follow=true no_ignore=true hidden=true", "find all files")
+map("ff", "frecency workspace=CWD ", "find files (frecency)")
 
-map(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "telescope find all files" }
-)
-
-map("n", "<leader>ff", "<cmd> Telescope frecency workspace=CWD <CR>", { desc = "find files (frecency)" })
-
-map("n", "<leader>re", function()
+vim.keymap.set("n", "<leader>re", function()
   require("telescope.builtin").registers { initial_mode = "normal" }
 end)
 
-map("n", "<leader><tab>", function()
+vim.keymap.set("n", "<leader><tab>", function()
   require("telescope.builtin").buffers { initial_mode = "normal" }
 end)
