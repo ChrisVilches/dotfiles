d1bbc71f0f3e226ac24982aacde90faf42d7abc8
Chris Vilches <felovilches@gmail.com>
2025-11-05 01:50:48 +0900

    scripts terminal-to-neovim.sh refactor

--- scripts/terminal-to-neovim.sh
+++ scripts/terminal-to-neovim.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/bin/sh
+# TODO: After committing the content changes, make another commit but
+# changing the script name (neovim -> editor) since now it's generic
+# and accepts vim too (and it's a bit faster).
 
-remove-empty-lines() {
-  sed -i '/^\s*$/d' $1
+trim-empty-lines() {
+  local remove_leading='/./,$!d'
+  local content=$(cat $1)
+  echo "$content" | sed "$remove_leading"| tac | sed "$remove_leading" | tac > $1
 }
 
 ID="$(date '+%b%d-%H%M%S')"
 DIR="/tmp/term-captures"
 mkdir -p $DIR
 FILE="$DIR/$ID"
-SCROLL_DOWN="lua vim.schedule(function() vim.cmd('normal! G') end)"
-EDITOR="nvim -c \"$SCROLL_DOWN\""
+# NVIM_SCROLL_DOWN="lua vim.schedule(function() vim.cmd('normal! G') end)"
+# EDITOR="nvim -c \"$NVIM_SCROLL_DOWN\""
+EDITOR="vim -c 'normal! G'"
 
 tmux capture-pane -S -1000
 tmux save-buffer $FILE
 
-remove-empty-lines $FILE
+trim-empty-lines $FILE
 
 tmux split-window -h "cd $DIR && $EDITOR $FILE"
+tmux resize-pane -Z
