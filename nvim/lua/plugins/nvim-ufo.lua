b64117835872f2e59ca2b8260a98315035f42ca1
Chris Vilches <felovilches@gmail.com>
2026-01-04 17:06:15 +0900

    nvim: config nvim-ufo (fold plugin) and learn some basic (but useful) usage

--- nvim/lua/plugins/nvim-ufo.lua
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -1,15 +1,14 @@
 local function config()
-  vim.o.foldcolumn = "1" -- '0' is not bad
+  -- NOTE: The auto session plugin may disable or change some of these options.
+
+  vim.o.foldcolumn = "0" -- '0' is not bad
   vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
   vim.o.foldlevelstart = 99
   vim.o.foldenable = true
 
-  vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-  vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
   require("ufo").setup {
     provider_selector = function(_, _, _)
-      return { "lsp", "indent" }
+      return { "treesitter", "indent" }
     end,
   }
 end
@@ -20,3 +19,9 @@ return {
   dependencies = { "kevinhwang91/promise-async" },
   config = config,
 }
+
+-- Main fold usage:
+-- za to fold a block
+-- za to unfold a block
+-- zR to unfold all
+-- There are many other ways to fold, but these are the simplest ways.
