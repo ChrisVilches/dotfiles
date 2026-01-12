e1a744dc032285660bb249c6c6a271e662d1822a
Chris Vilches <felovilches@gmail.com>
2026-01-12 13:22:35 +0900

    nvim: buffer closing, remove stack custom plugin

--- nvim/custom-plugins/lua/buffer-close/init.lua
+++ nvim/custom-plugins/lua/buffer-close/init.lua
@@ -1,86 +1,78 @@
 local M = {}
 
-local closed_files = require("stack").Stack:new()
+local closed_buffers = {}
 
-local function delete_buf_and_arrange_tabs(bufnr)
-  local buf = require "bufferline"
-  buf.move(1)
-  buf.cycle(-1)
-  vim.api.nvim_buf_delete(bufnr, { force = true })
-end
+local function save_closed_buffer(filepath)
+  local entry = vim.tbl_filter(function(item)
+    return item.file_path == filepath
+  end, closed_buffers)[1]
 
-local function print_needs_to_save(file_path)
-  local name = file_path
-  if name == nil or name == "" then
-    name = "unnamed"
-  end
-  vim.api.nvim_err_writeln("Needs to save before closing a buffer (" .. name .. ")")
-end
-
-local function push_history(file_path, view)
-  closed_files:push { file_path, view }
-end
-
-local function close_aux(force)
-  local bufnr = vim.fn.bufnr()
-  local file_path = vim.api.nvim_buf_get_name(bufnr)
-  local is_unnamed = file_path == nil or file_path == ""
-  local is_modified = vim.bo[bufnr].modified
-  local view = vim.fn.winsaveview()
-
-  if is_modified and not force then
-    print_needs_to_save(file_path)
-    return
-  end
-
-  delete_buf_and_arrange_tabs(bufnr)
-
-  if not is_unnamed then
-    push_history(file_path, view)
+  if entry == nil then
+    table.insert(closed_buffers, filepath)
   end
 end
 
-local current_tabs_buffers = function()
-  -- More information
-  -- :h buftype
+function M.listed_buffers()
   return vim.tbl_filter(function(buf)
     return vim.bo[buf].buflisted and vim.bo[buf].buftype == ""
   end, vim.api.nvim_list_bufs())
 end
 
-function M.close_other_except_unsaved()
-  local current_buf = vim.api.nvim_get_current_buf()
+function M.listen_closed_buffers()
+  vim.api.nvim_create_autocmd("BufDelete", {
+    callback = function(ev)
+      local name = vim.api.nvim_buf_get_name(ev.buf)
+      local should_add_to_history = vim.bo[ev.buf].buflisted and vim.bo[ev.buf].buftype == "" and name ~= ""
 
-  for _, buf in ipairs(current_tabs_buffers()) do
-    if buf ~= current_buf and not vim.bo[buf].modified then
-      vim.api.nvim_buf_delete(buf, {})
-    end
-  end
+      if should_add_to_history then
+        save_closed_buffer(vim.fn.fnamemodify(name, ":."))
+      end
+    end,
+  })
 end
 
-function M.close()
-  close_aux(false)
-end
-
-function M.close_force()
-  close_aux(true)
-end
-
-function M.reopen_last()
-  if closed_files:is_empty() then
-    vim.api.nvim_err_writeln "No files to re-open"
+function M.reopen()
+  if #closed_buffers == 0 then
+    vim.notify("No closed buffers to reopen", vim.log.levels.WARN)
     return
   end
 
-  local file_path, view = unpack(closed_files:pop())
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local conf = require("telescope.config").values
+  local actions = require "telescope.actions"
+  local action_state = require "telescope.actions.state"
 
-  if vim.uv.fs_stat(file_path) == nil then
-    vim.api.nvim_err_writeln("Cannot re-open file: " .. file_path)
-    return
-  end
-
-  vim.cmd("edit " .. vim.fn.fnameescape(file_path))
-  vim.fn.winrestview(view)
+  pickers
+    .new({}, {
+      prompt_title = "Closed Buffers",
+      sorter = conf.generic_sorter {},
+      finder = finders.new_table {
+        results = closed_buffers,
+        entry_maker = function(entry)
+          return { value = entry, display = entry, ordinal = entry }
+        end,
+      },
+      previewer = conf.file_previewer {},
+      attach_mappings = function(_, map)
+        map({ "i", "n" }, "<CR>", function(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          if selection then
+            vim.cmd("buffer " .. selection.value)
+            vim.bo.buflisted = true
+            for i, entry in ipairs(closed_buffers) do
+              if entry == selection.value then
+                table.remove(closed_buffers, i)
+                break
+              end
+            end
+          end
+        end)
+        return true
+      end,
+    })
+    :find()
 end
 
 return M
--- nvim/lua/mappings/navigation.lua
+++ nvim/lua/mappings/navigation.lua
@@ -14,13 +14,22 @@ map("n", "<c-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "tab next" })
 map("n", "<leader><c-h>", "<cmd>BufferLineMovePrev<CR>", { desc = "buffer move prev" })
 map("n", "<leader><c-l>", "<cmd>BufferLineMoveNext<CR>", { desc = "buffer move next" })
 
-map(
-  "n",
-  "<leader>tx",
-  require("buffer-close").close_other_except_unsaved,
-  { desc = "close all tabs except current and unsaved" }
-)
+require("buffer-close").listen_closed_buffers()
 
-map("n", "<leader>x", require("buffer-close").close, { desc = "buffer close" })
-map("n", "<leader>X", require("buffer-close").close_force, { desc = "buffer close (force)" })
-map("n", "<leader>tr", require("buffer-close").reopen_last, { desc = "tab re-open" })
+map("n", "<leader>tx", function()
+  local curr_bufnr = vim.api.nvim_get_current_buf()
+
+  -- TODO: What happens with unnamed ones?
+  for _, buf in ipairs(require("buffer-close").listed_buffers()) do
+    if curr_bufnr ~= buf and vim.bo[buf].modified then
+      vim.notify("Some buffers are unsaved", vim.log.levels.WARN)
+      return
+    end
+  end
+
+  vim.cmd "BufferLineCloseOthers"
+end, { desc = "close other tabs if none are unsaved" })
+
+map("n", "<leader>x", "<cmd>bd<cr>", { desc = "buffer close" })
+map("n", "<leader>X", "<cmd>bd!<cr>", { desc = "buffer close (force)" })
+map("n", "<leader>tr", require("buffer-close").reopen, { desc = "tab re-open" })
