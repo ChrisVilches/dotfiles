8e8e4d05a9ea358aafe2bdcc40b553892cdcbb9f
Chris Vilches <felovilches@gmail.com>
2026-03-09 08:38:45 +0900

    nvim: improve explorer auto close mechanism

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -6,18 +6,16 @@ end
 
 local function explorer_configure_auto_close(picker)
   -- NOTE: Found this "on" in snacks source code.
-  -- This closes the explorer when focus shifts away by checking if the filetype
-  -- changes from "snacks_*". The input within the explorer is a separate window,
-  -- so entering insert mode (for file search) triggers WinLeave. We only close
-  -- the explorer when focus leaves all Snacks GUI elements. Opening other pickers like
-  -- command history or word grep won't close the explorer, which is useful for
-  -- executing commands or searching command history within the explorer.
   picker.layout.root:on("WinLeave", function()
     vim.schedule(function()
       local next_win = vim.api.nvim_get_current_win()
-      local buf = vim.api.nvim_win_get_buf(next_win)
-      local filetype = vim.bo[buf].filetype
-      if not filetype:match "^snacks_" then
+
+      -- Windows inside the explorer that are whitelisted and don't close the explorer when focused.
+      local input = picker.layout.wins.input.win
+      local list = picker.layout.wins.list.win
+      local inside_picker = next_win == input or next_win == list
+
+      if not inside_picker then
         picker:close()
       end
     end)
