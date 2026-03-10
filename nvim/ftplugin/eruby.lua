76969deb30ce0e58958e19fa5833066df1d5d714
Chris Vilches <felovilches@gmail.com>
2026-03-10 23:55:42 +0900

    nvim: rename ftplugin rhtml -> eruby

--- nvim/ftplugin/eruby.lua
+++ nvim/ftplugin/eruby.lua
@@ -1,5 +1,3 @@
--- TODO: This opens the file with absolute path, which makes it too long.
--- Compare the original path with the newly opened one, and the new one is longer.
 local function toggle_lang_view()
   local abs_path = vim.api.nvim_buf_get_name(0)
   if abs_path == "" then
@@ -20,13 +18,12 @@ local function toggle_lang_view()
   end
 
   if vim.fn.filereadable(alt_path) == 1 then
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0)) -- current cursor position
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
     vim.cmd("edit " .. vim.fn.fnameescape(alt_path))
-    -- Restore line (and same column too)
     pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
   else
     vim.notify("Counterpart file does not exist: " .. alt_path, vim.log.levels.ERROR)
   end
 end
 
-vim.api.nvim_create_user_command("ToggleLangView", toggle_lang_view, {})
+vim.api.nvim_buf_create_user_command(0, "ToggleLangView", toggle_lang_view, {})
