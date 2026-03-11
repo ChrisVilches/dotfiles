5fb68ed8cab9848e85647bd863cb9eb8452fe51a
Chris Vilches <felovilches@gmail.com>
2026-03-12 01:07:36 +0900

    nvim: fix bug

--- nvim/custom-plugins/lua/sessions/init.lua
+++ nvim/custom-plugins/lua/sessions/init.lua
@@ -70,7 +70,7 @@ end
 local function clean_empty_windows()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     local buf = vim.api.nvim_win_get_buf(win)
-    if vim.api.nvim_buf_get_name(buf) == "" then
+    if vim.api.nvim_buf_get_name(buf) == "" and #vim.api.nvim_list_wins() > 1 then
       vim.api.nvim_win_close(win, true)
     end
   end
