f6e97d6528529976d24ee7bc9946ccff5b9806dc
Chris Vilches <felovilches@gmail.com>
2026-07-31 07:28:15 +0900

    zsh: revamp extra (cd hooks), it's now much simpler

--- zsh-extra.zsh
+++ zsh-extra.zsh
@@ -1,92 +1,31 @@
-typeset -g AUTO_ENV_ACTIVE=""
-typeset -g AUTO_ENV_PROVIDER=""
+auto_env() {
+    #
+    # Python virtual environment
+    #
 
-# TODO: Works for python (enabling/disabling) but how would I do this for Ruby
-# and Node? Disabling it would mean go back to the global/standard version, but
-# not sure if fnm and rbenv have that. Just vibecode some solution.
-
-# -----------------------------------------------------------------------------
-# Python virtual environments
-# -----------------------------------------------------------------------------
-
-__auto_python_venv() {
-    local dir="$PWD"
-
-    while [[ "$dir" != "/" ]]; do
-        for name in .venv venv env; do
-            if [[ -f "$dir/$name/bin/activate" ]]; then
-                REPLY="$dir/$name"
-                return 0
-            fi
-        done
-
-        dir="${dir:h}"
+    for name in .venv venv env; do
+        if [[ -f "$PWD/$name/bin/activate" ]]; then
+            source "$PWD/$name/bin/activate"
+            printf "🌱 venv: %s\n" "$PWD/$name"
+            break
+        fi
     done
 
-    return 1
-}
-
-# -----------------------------------------------------------------------------
-# Main dispatcher
-# -----------------------------------------------------------------------------
-
-auto_env() {
-    local found=""
-    local provider=""
-
     #
-    # Try each provider.
+    # Node (fnm)
     #
 
-    if __auto_python_venv; then
-        found="$REPLY"
-        provider="python-venv"
+    if [[ -f "$PWD/.nvmrc" ]]; then
+        fnm use
     fi
 
     #
-    # Nothing changed.
+    # Ruby (rbenv)
     #
 
-    if [[ "$AUTO_ENV_ACTIVE" == "$found" ]]; then
-        return
-    fi
-
-    #
-    # Deactivate previous environment.
-    #
-
-    if [[ -n "$AUTO_ENV_ACTIVE" ]]; then
-        printf "🌱 Deactivating %s: %s\n" \
-            "$AUTO_ENV_PROVIDER" \
-            "$AUTO_ENV_ACTIVE"
-
-        case "$AUTO_ENV_PROVIDER" in
-            python-venv)
-                (( $+functions[deactivate] )) && deactivate
-                ;;
-        esac
-
-        AUTO_ENV_ACTIVE=""
-        AUTO_ENV_PROVIDER=""
-    fi
-
-    #
-    # Activate new environment.
-    #
-
-    if [[ -n "$found" ]]; then
-        printf "🌱 Activating %s: %s\n" \
-            "$provider" \
-            "$found"
-
-        case "$provider" in
-            python-venv)
-                source "$found/bin/activate"
-                ;;
-        esac
-
-        AUTO_ENV_ACTIVE="$found"
-        AUTO_ENV_PROVIDER="$provider"
+    if [[ -f "$PWD/.ruby-version" ]]; then
+        rbenv local "$(<"$PWD/.ruby-version")"
+        printf "rbenv: %s\n" "$(rbenv version)"
     fi
 }
 
